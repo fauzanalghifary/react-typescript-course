7c6c348525d9541148c53b9e89e6304e929bf707
fauzanalghifary <m.fauzanghifary@gmail.com>
2023-01-21 21:26:53 +0700

    continue section 26

---
+++ Section 26 Basic Typescript/features/annotations/function.ts
@@ -0,0 +1,32 @@
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const subtract = (a: number, b: number): number => {
+  return a - b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
+
+// Void and Never
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+const throwError2 = (message: string): string => {
+  if (!message) {
+    throw new Error(message);
+  }
+  return message;
+};
