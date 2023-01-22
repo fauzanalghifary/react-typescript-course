a9e70704daaaa3263c6c60ffc4a1d9fe72d91b3c
fauzanalghifary <m.fauzanghifary@gmail.com>
2023-01-22 12:01:48 +0700

    continue section 26

--- Section 26 Basic Typescript/features/annotations/function.ts
+++ Section 26 Basic Typescript/features/annotations/function.ts
@@ -30,3 +30,23 @@ const throwError2 = (message: string): string => {
   }
   return message;
 };
+
+// Destructuring with Annotations
+
+const todaysWeather = {
+  date: new Date(),
+  weather: "sunny",
+};
+
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(todaysWeather);
---
+++ Section 26 Basic Typescript/features/array.ts
@@ -0,0 +1,25 @@
+const carMakers = ["ford", "toyota", "chevy"];
+const dates = [new Date(), new Date()];
+
+const carsByMake = [["f150"], ["corolla"], ["camaro"]];
+
+// Help with inference when extracting values
+
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+
+// carMakers.push(100);
+
+// Help with 'map'
+
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
+
+// Flexible types
+
+const importantDates: (Date | string)[] = [new Date()];
+importantDates.push("2030-10-10");
+importantDates.push(new Date());
