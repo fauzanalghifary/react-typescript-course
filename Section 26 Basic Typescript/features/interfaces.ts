d25f83df3c020235940478e865d8b234a0827445
fauzanalghifary <m.fauzanghifary@gmail.com>
2023-01-23 20:50:57 +0700

    continue section 26

--- Section 26 Basic Typescript/features/interfaces.ts
+++ Section 26 Basic Typescript/features/interfaces.ts
@@ -11,8 +11,18 @@ const oldCivic = {
   },
 };
 
+const drink = {
+  color: "brown",
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar`;
+  },
+};
+
 const printSummary = (item: Reportable): void => {
   console.log(`Name: ${item.summary()}`);
 };
 
 printSummary(oldCivic);
+printSummary(drink);
