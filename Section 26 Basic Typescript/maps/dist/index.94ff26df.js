d6e7c36b034967af505af64139bf41de1c89445a
fauzanalghifary <m.fauzanghifary@gmail.com>
2023-01-25 06:53:17 +0700

    continue section 26

--- Section 26 Basic Typescript/maps/dist/index.94ff26df.js
+++ Section 26 Basic Typescript/maps/dist/index.94ff26df.js
@@ -1,3 +1,12 @@
-console.log("HI");
+/// <reference types="@types/google.maps" />
+// import { User } from "./User";
+// import { Company } from "./Company";
+const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
 
 //# sourceMappingURL=index.94ff26df.js.map
--- Section 26 Basic Typescript/maps/src/index.ts
+++ Section 26 Basic Typescript/maps/src/index.ts
@@ -1,1 +1,6 @@
-console.log("HI");
+/// <reference types="@types/google.maps" />
+import { User } from "./User";
+import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
+
+new CustomMap("map");
